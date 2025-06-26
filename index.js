5b0c5e84c88ad5220cc920643239092ca778ae0d
karl <karl.fahlbusch7241@gmail.com>
2025-06-26 13:09:27 -0500

    create css and js, start js

---
+++ index.js
@@ -0,0 +1,7 @@
+const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT = "/2506-Karl";
+const RESOURCE = "/events";
+const API = BASE + COHORT + RESOURCE;
+
+let parties = [];
+let selectedParty;
