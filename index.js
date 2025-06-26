fdc681398b7f9028b002a09026da78bc1085f0c2
karl <karl.fahlbusch7241@gmail.com>
2025-06-26 15:59:46 -0500

    js update

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ const API = BASE + COHORT + RESOURCE;
 
 let parties = [];
 let selectedParty;
+let rsvp = [];
+let guests = [];
+
+async function getParties() {
+  try {
+    const response = await fetch(`${API}/${events}`);
+    const result = await response.json();
+    parties = result.data;
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getParty(id) {
+  try {
+    const response = await fetch(`${API}/${events}/${id}`);
+    const result = await response.json();
+    selectedParty = result.data;
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+}
